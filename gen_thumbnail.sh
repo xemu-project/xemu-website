534be1edea2f4829f7c2f4e3fed09cde4de45a17
Matt Borgerson <contact@mborgerson.com>
2020-10-26 13:40:18 -0700

    Just use `convert` in thumbnail script

--- gen_thumbnail.sh
+++ gen_thumbnail.sh
@@ -1,12 +1,9 @@
 #!/bin/bash -ex
 INPUT=$1
-OUTPUT_RESIZED=`dirname $1`/cover_front_thumbnail_tmp.png
 OUTPUT_COMPRESSED=`dirname $1`/cover_front_thumbnail.jpg
 if [[ -e "$OUTPUT_COMPRESSED" ]]; then
 	echo "$OUTPUT_COMPRESSED already exists"
 	exit 0
 fi
 echo "CONVERTING $INPUT"
-convert $INPUT -quality 100 -resize 256 -density 72x72 $OUTPUT_RESIZED
-./guetzli/bin/Release/guetzli $OUTPUT_RESIZED $OUTPUT_COMPRESSED
-rm $OUTPUT_RESIZED
+convert $INPUT -quality 85 -resize 256 -density 72x72 $OUTPUT_COMPRESSED
