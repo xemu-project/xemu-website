b1fce8b52ada46f0fb18776bb71c2085a7e9ee18
Matt Borgerson <contact@mborgerson.com>
2023-01-06 20:41:15 -0700

    Migrate GitHub references in generate.py

--- generate.py
+++ generate.py
@@ -18,7 +18,7 @@ from tqdm import tqdm
 from minify_html import minify as minify_html
 
 output_dir = 'dist'
-repo_url_base = 'https://raw.githubusercontent.com/mborgerson/xemu-website/master/'
+repo_url_base = 'https://raw.githubusercontent.com/xemu-project/xemu-website/master/'
 compatibility_reports_url = 'https://reports.xemu.app/compatibility'
 compatibility_reports_url_verify_certs = True
 main_url_base = 'https://xemu.app'
@@ -69,7 +69,7 @@ class Issue:
             return
         titles_re = re.compile(r'Titles?[:/]\s*([a-fA-f0-9,\s]+)', re.IGNORECASE)
         title_id_re = re.compile(r'([a-fA-f0-9]{8})')
-        for issue in Github().get_user('mborgerson').get_repo('xemu').get_issues(state='all'):
+        for issue in Github().get_user('xemu-project').get_repo('xemu').get_issues(state='all'):
             # Look for a titles sequence and pull out anything that looks like
             # an id
             references = ' '.join(titles_re.findall(issue.body or ''))
@@ -274,8 +274,8 @@ def main():
         xemu_build_version = '0.7.55'
         xemu_build_date = datetime(2021, 6, 4, 19, 13, 6)
     else:
-        xemu_build_version = requests.get('https://raw.githubusercontent.com/mborgerson/xemu/ppa-snapshot/XEMU_VERSION').text
-        latest_release = Github().get_user('mborgerson').get_repo('xemu').get_latest_release()
+        xemu_build_version = requests.get('https://raw.githubusercontent.com/xemu-project/xemu/ppa-snapshot/XEMU_VERSION').text
+        latest_release = Github().get_user('xemu-project').get_repo('xemu').get_latest_release()
         xemu_build_date = latest_release.created_at
 
     print('Rebuilding index...')
